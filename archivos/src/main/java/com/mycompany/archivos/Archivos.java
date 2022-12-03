5b277b436d8bee077f5067a98f37250072f5dad5
JesseJaisenber <115435580+JesseJaisenber@users.noreply.github.com>
2022-12-02 23:10:04 -0600

    Add files via upload

---
+++ archivos/src/main/java/com/mycompany/archivos/Archivos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.archivos;
+
+/**
+ *
+ * @author bernardez03
+ */
+public class Archivos {
+
+    public static void main(String[] args) {
+        
+        
+    } 
+}
